701c0cd871db5d65843fac68628dc06a5d0f6c5b
etnguyen84 <etnguyen84@E1117W08>
2019-11-12 15:29:27 -0600

    web pages

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+var squares = document.getElementsByClassName("square")
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', onClick);
+}
+
+var turnElement = document.getElementById("turn");
+turnElement.innerText = "Player " + currentTurn + "'s Turn"; // `Player's ${currentTurn}'s Turn.`
+
+function onClick(event) {
+    event.preventDefault();
+    event.target.innerText = currentTurn;
+    nextTurn();
+}
+
+function nextTurn(event) {
+    currentTurn = (currentTurn === "X") ? "O" : "X";
+    turnElement.innerText = "Player " + currentTurn + "'s Turn"; // `Player's ${currentTurn}'s Turn.`
+}
